6c99c8a64e6c7ac93a5e030fde6fb9076653f01f
p33t <1179301+p33t@users.noreply.github.com>
2023-05-30 14:36:55 -0700

    Include Schema XML Docs in Swagger UI.

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Learn ASP6 - webapi",
+        Version = "v1",
+        Description = "Illustration of how to setup an ASP 6 web API"
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
--- webapi/WeatherForecast.cs
+++ webapi/WeatherForecast.cs
@@ -1,12 +1,28 @@
 namespace webapi;
 
+/// <summary>
+/// Weather forecast for a specific day.<br/>
+/// Yes, XML Doc markup <b>Works!</b>
+/// </summary>
 public class WeatherForecast
 {
+    /// <summary>
+    /// Date being  forecasted
+    /// </summary>
     public DateTime Date { get; set; }
 
+    /// <summary>
+    /// Forecasted temperature in Celsius
+    /// </summary>
     public int TemperatureC { get; set; }
 
+    /// <summary>
+    /// Forecasted temperature in Fahrenheit
+    /// </summary>
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+    /// <summary>
+    /// Summary of forecasted weather
+    /// </summary>
     public string? Summary { get; set; }
 }
